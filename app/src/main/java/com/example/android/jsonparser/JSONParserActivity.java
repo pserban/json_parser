c2dee8a7234addfed52ab402ba7aa6d4ca6df012
Serban Patriciu <serban_patriciu@hotmail.com>
2022-04-25 10:58:59 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/jsonparser/JSONParserActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.jsonparser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JSONParserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_json_parser);
+    }
+}
